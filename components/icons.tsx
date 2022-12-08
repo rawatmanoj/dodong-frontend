e945bd7d176dab7be27a1772ed318ff38835e30d
rawatmanoj <rawatmanoj997@gmail.com>
2022-12-08 23:15:09 +0530

    added login pages and fonts and other config

---
+++ components/icons.tsx
@@ -0,0 +1,12 @@
+import {
+    AiFillGithub
+  } from "react-icons/ai"
+import {
+  FaSpinner
+  } from "react-icons/fa"
+
+  export const Icons = {
+    gitHub: AiFillGithub,
+    spinner:FaSpinner
+  }
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { colors } = require("tailwindcss/colors")
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
@@ -6,7 +8,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        darkteal: {
+
+        },
+        orange: {
+          50: "#FFF5E6",
+          100: "#FFDFB0",
+          200: "#FFD08A",
+          300: "#FFBA54",
+          400: "#FFAD33",
+          500: "#FF9800",
+          600: "#E88A00",
+          700: "#B56C00",
+          "CC7A00": "#CC7A00",
+          "593500": "#593500"
+        },
+        neutral: {
+          50: "#E9EBF8",
+          100: "#D8D8D8",
+          200: "#C5C5C5",
+          300: "#AAAAAA",
+          400: "#999999",
+          500: "#808080",
+          600: "#747474",
+          700: "#5B5B5B",
+        },
+        "734400": "#734400"
+      }
+    },
   },
   plugins: [],
 }
